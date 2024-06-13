51d0c4312626b73991c3d686e1f3fc86b4acdf5c
Yael Correa <yscorrea10@gmail.com>
2024-06-13 15:29:39 -0300

    avance proyectos en portfolio

---
+++ src/components/Sections/Section2/ProjectItem/index.jsx
@@ -0,0 +1,85 @@
+import { 
+    Box,
+    Typography
+ } from "@mui/material";
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+
+export default function ProjectItem ({img, title}) {
+    return (
+    <Box
+      sx={{
+        position: 'relative',
+        overflow: 'hidden',
+        borderRadius: '5px',
+        '&:hover .hover-content-top': {
+          display: 'block',
+        },
+        '&:hover .hover-content-bottom': {
+          display: 'block',
+        },
+        '&::after': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          transition: 'background-color 0.3s ease',
+          zIndex: 1,
+        },
+        '&:hover::after': {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+        '& img': {
+          borderBottomLeftRadius: 4,
+          borderBottomRightRadius: 4,
+          display: 'block',
+          width: '100%',
+          transition: 'transform 0.3s ease',
+          zIndex: 0,
+          position: 'relative',
+        },
+        '&:hover img': {
+          transform: 'scale(1.05)',
+        },
+      }}
+    >
+      <Box
+        className="hover-content-top"
+        sx={{
+          position: "absolute",
+          top: "0",
+          right: "0",
+          zIndex: 2,
+          display: "none",
+          padding: "1em",
+          color: "white",
+        }}
+      >
+        <ArrowOutwardIcon />
+      </Box>
+      <Box
+        className="hover-content-bottom"
+        sx={{
+          position: "absolute",
+          bottom: "0",
+          left: "0",
+          zIndex: 2,
+          display: "none",
+          padding: "1em",
+          color: "white",
+        }}
+      >
+        <Typography variant="h6">
+          {title}
+        </Typography>
+      </Box>
+      <img
+        src={img}
+        alt=""
+        loading="lazy"
+      />
+    </Box>
+    )
+}
--- src/components/Sections/Section2/index.jsx
+++ src/components/Sections/Section2/index.jsx
@@ -2,6 +2,8 @@ import {
     Box,
     Typography
 } from "@mui/material"
+import Masonry from '@mui/lab/Masonry';
+import ProjectItem from "./ProjectItem";
 
 export default function Section2 () {
     return (
@@ -11,6 +13,17 @@ export default function Section2 () {
                 margin: {xs: "1em 0 1em 1.5em", sm: "2em 0 1em 2em"},
                 fontSize: {xs: "32px", md: "64px"}
             }}>— Portfolio</Typography>
+            <Box display="flex" justifyContent="center">
+                <Masonry columns={{xs: 1, sm: 2, md: 3}} spacing={2} sx={{width: "60%"}}>
+                    <ProjectItem img="https://res.cloudinary.com/dkfa8olux/image/upload/v1718302284/yael_portfolio_2024/projects/portfolio-2024/Captura_de_pantalla_995_uj8xwf.png" title="Portfolio 2024" />
+                    <ProjectItem img="https://res.cloudinary.com/dkfa8olux/image/upload/v1714686914/yael_portfolio_2024/projects/funkoshop/uwvnvpo5vwd9iev1pbpe.png" title="Funkoshop"/>
+                    <ProjectItem img="https://res.cloudinary.com/dkfa8olux/image/upload/v1718302212/yael_portfolio_2024/projects/Summer%20Exploring/screencapture-127-0-0-1-5500-index-html-2024-06-06-15_34_00_ybsljl.png" title="Summer Exploring" />
+                    <ProjectItem img="https://res.cloudinary.com/dkfa8olux/image/upload/v1718302658/yael_portfolio_2024/projects/wwwapas/Captura_de_pantalla_996_vgg0ai.png" title="wwwapas"/>
+                    <ProjectItem img="https://res.cloudinary.com/dkfa8olux/image/upload/v1718301427/yael_portfolio_2024/projects/ECOSistema/Captura_de_pantalla_956_lfeemz.png" title="ECOSistema"/>
+                    <ProjectItem img="https://res.cloudinary.com/dkfa8olux/image/upload/v1718303064/yael_portfolio_2024/projects/calculadora-web/Desktop_-_3_wa1o2r.png" title="Calculadora web"/>
+                    <ProjectItem img="https://res.cloudinary.com/dkfa8olux/image/upload/v1718303289/yael_portfolio_2024/projects/portfolio-2023/imagen-webflow_cq4my4.png" title="Portfolio 2023"/>
+                </Masonry>
+            </Box>
         </Box>
     )
 }
